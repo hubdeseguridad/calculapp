01b943f1b88874cf070793e4235b0c6be0614bde
joksantelles <ioksan.telles@gmail.com>
2025-08-07 13:52:50 -0600

    Frontend latest update

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const BASE_URL = import.meta.env.VITE_API_URL || "https://calculadora.joksantelles.com/api/v1";
 
+
 export const apiClient = axios.create({
     baseURL: BASE_URL,
     headers: {
